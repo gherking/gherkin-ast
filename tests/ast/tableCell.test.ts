0cd20e490c225eeeb928d16085b499ba5bd85624
Laszlo Szikszai <sziklaszlo@gmail.com>
2019-04-01 17:18:31 +0200

    Jest fix

---
+++ tests/ast/tableCell.test.ts
@@ -0,0 +1,17 @@
+import { TableCell } from "../../src"
+
+describe("TableCell", () => {
+    test("should create model of a TableCell", () => {
+        throw new Error("ERROR");
+    });
+
+    test("should have a method to clone it");
+
+    test("should have method to replace value");
+
+    describe("parse", () => {
+        test("should throw error if not GherkinTableCell is passed");
+
+        test("should parse GherkinTableCell");
+    })
+});
