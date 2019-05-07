9003be8379f1b48a25861c8cc2b6f68d6af4b7e0
sandor_orosz <SsamcroO#8989>
2019-05-07 16:16:47 +0200

    backGround tests

---
+++ tests/ast/backGround.test.ts
@@ -0,0 +1,52 @@
+import { Background } from "../../src";
+import { GherkinBackground } from "../../src/gherkinObject";
+
+describe("Background", () => {
+    test("should create model of a Background", () => {
+        // Given
+        // When
+        const backGround: Background = new Background("Bkeeyword", "Bname", "Bdesc");
+        // Then
+        expect(backGround).toBeDefined();
+        expect(backGround.keyword).toEqual("Bkeeyword");
+    });
+
+    test("should have a method to clone it", () => {
+        // Given
+        const backGroundA: Background = new Background("Bkeeyword", "Bname", "Bdesc");
+        // When
+        const backGroundB: Background = backGroundA.clone();
+        // Then
+        expect(backGroundB.keyword).toEqual(backGroundA.keyword);
+        expect(backGroundB.name).toEqual(backGroundA.name);
+        expect(backGroundB.description).toEqual(backGroundA.description);
+        expect(backGroundB).not.toBe(backGroundA);
+    });
+
+    describe("parse", () => {
+        test("should throw error if not GherkinBackground is passed", () => {
+            // Given
+            const obj: GherkinBackground = {} as GherkinBackground;
+            // When
+            // Then
+            expect(() => Background.parse(obj)).toThrow();
+        });
+
+        test("should parse GherkinBackground", () => {
+            // Given
+            const obj: GherkinBackground = {
+                background: {
+                    keyword: "Bkeeyword",
+                    name: "Bname",
+                    steps: [],
+                },
+            } as GherkinBackground;
+            // When
+            const bg: Background = Background.parse(obj);
+            // Then
+            expect(bg).toBeDefined();
+            expect(bg.keyword).toEqual("Bkeeyword");
+            expect(bg.name).toEqual("Bname");
+        });
+    });
+});
